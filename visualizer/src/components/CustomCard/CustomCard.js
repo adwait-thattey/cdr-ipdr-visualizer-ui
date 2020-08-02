f6251951397a975d01e076fe3b3750952873f126
b30wulffz <shlokpandey123@gmail.com>
2020-08-03 04:17:55 +0530

    Dashboard structure done

--- visualizer/src/components/CustomCard/CustomCard.js
+++ visualizer/src/components/CustomCard/CustomCard.js
@@ -3,7 +3,7 @@ import styles from './CustomCard.module.scss';
 
 const CustomCard = (props) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={props.onClick}>
       <div className={styles.cardIcon}>{props.icon}</div>
       <div className={styles.cardText}>{props.title}</div>{' '}
     </div>
