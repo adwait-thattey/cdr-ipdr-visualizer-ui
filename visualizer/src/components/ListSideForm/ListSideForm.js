2f2a7708214ab0549a31f2d836490a3f9fc724a9
Adwait Thattey <coderdude1999@gmail.com>
2020-08-02 14:08:55 +0530

    lists page in upload page

---
+++ visualizer/src/components/ListSideForm/ListSideForm.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+const Demo = ({obj}) => {
+
+  console.log("object form", obj)
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ watchlistdata: obj['content'] }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Watch List Name"
+        name="watchlistname"
+        rules={[{ required: true, message: 'Please input a name for watchlist!' }]}
+      >
+        <Input defaultValue={obj.name}/>
+      </Form.Item>
+
+      <Form.Item
+        label="Watchlist Data"
+        name="watchlistdata"
+        rules={[{ required: true, message: 'Please input data, each entry of the form type,object' }]}
+      >
+        <Input.TextArea rows={10}/>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Save
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Demo;
---
+++ visualizer/src/components/UploadListButton/UploadListButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListItem = ({ name, id }) => {
+    return (
+        <div>
+            <div>{key}</div>
+            <div>{value}</div>
+        </div>
+    );
+};
+
+export default ListItem;
