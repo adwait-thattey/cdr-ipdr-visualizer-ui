a7017b1ff03c855232be65de51834ffd65541b63
b30wulffz <shlokpandey123@gmail.com>
2020-08-03 00:05:35 +0530

    Recode List Page and add routes for tabs

--- visualizer/src/containers/Dashboard/Dashboard.js
+++ visualizer/src/containers/Dashboard/Dashboard.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from './Dashboard.module.scss';
+import Header from '../../components/Header/Header';
 
 const Dashboard = () => {
-  return <h1>Dashboard TODO</h1>;
+  return (
+    <div className={styles.container}>
+      <Header title="Dashboard" onFilterClick={() => {}} />
+      <div className={styles.section}></div>
+    </div>
+  );
 };
 
 export default Dashboard;
